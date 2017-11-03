13501eca1644072d4739c85df3020202fdf3b5b6
Mateusz Winiarski <mateusz.winiarski.94@gmail.com>
2017-11-03 12:58:45 +0100

    Add WeatherStationListView to WeatherView

--- RasPiHomeDock/Utils/WeatherStationList.cs
+++ RasPiHomeDock/Utils/WeatherStationList.cs
@@ -1,10 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.ObjectModel;
 using System.Linq;
 using RasPiHomeDock.Utils.Exceptions;
 
 namespace RasPiHomeDock.Utils {
-    public class WeatherStationList : List<WeatherStation> {
+    public class WeatherStationList : ObservableCollection<WeatherStation> {
         public WeatherStation this[string station]
             => this.FirstOrDefault(s => s.Station == station) ?? throw new NoSuchStationException();
     }
--- RasPiHomeDock/Views/WeatherView.xaml.cs
+++ RasPiHomeDock/Views/WeatherView.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
@@ -12,6 +13,7 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using RasPiHomeDock.Utils;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
 
@@ -20,12 +22,30 @@ namespace RasPiHomeDock.Views {
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
     public sealed partial class WeatherView : Page {
-        public WeatherView() { this.InitializeComponent(); }
+        public WeatherView() {
+            this.InitializeComponent();
+        }
 
-        protected override void OnNavigatedFrom(NavigationEventArgs e) {
-            if ( e.Parameter is TextBlock headerTextBlock ) {
-                headerTextBlock.Text = "Pogoda";
-            }
+        protected override void OnNavigatedTo(NavigationEventArgs e) {
+            base.OnNavigatedTo(e);
+
+            if ( e.Parameter is TextBlock headerTextBlock ) headerTextBlock.Text = "Pogoda";
+
+            StationListView.ItemsSource = weatherStations_;
+            if (weatherStations_.Count == 0) RefreshWeatherStationListBox();
+        }
+
+        private async void RefreshWeatherStationListBox() {
+            weatherStations_.Clear();
+
+            foreach ( WeatherStation station in await WeatherStation.GetAllStations() )
+                weatherStations_.Add(station);
+        }
+
+        private readonly WeatherStationList weatherStations_ = new WeatherStationList();
+
+        private void RefreshButton_OnClick(object sender, RoutedEventArgs e) {
+            RefreshWeatherStationListBox();
         }
     }
 }
